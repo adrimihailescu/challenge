80e7b34b357a92bb4e1319ba43615ed2778481f9
adriana <adriana.mihalescu@gmail.com>
2022-03-17 17:21:40 +0000

    add new component MenuSummary- header

--- src/client/App.js
+++ src/client/App.js
@@ -1,24 +1,11 @@
 import React from "react";
 import "./App.css";
-import ProductsMenu from "./components/ProductsMenu";
 import MenuBuilder from "./components/MenuBuilder";
+import MenuSummary from "./components/MenuSummary/MenuSummary";
 
 export default () => (
 	<div className="wrapper">
-		<div className="menu-summary">
-			<div className="container">
-				<div className="row">
-					<div className="col-6 menu-summary-left">
-						<span>6 items</span>
-					</div>
-					<div className="col-6 menu-summary-right">
-						6x <span className="dietary">ve</span>
-						4x <span className="dietary">v</span>
-						12x <span className="dietary">n!</span>
-					</div>
-				</div>
-			</div>
-		</div>
+		<MenuSummary />
 		<MenuBuilder />
 	</div>
 );
